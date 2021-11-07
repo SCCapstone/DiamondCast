72feb485eb20a560259f93d623ca4311cea9e66b
Frank Habersham <frankh@email.sc.edu>
2021-11-07 16:19:42 -0500

    Created create account 1 and 2 activities

---
+++ DiamondCastApp/app/src/main/java/com/example/diamondcastapp/CreateAccount2.java
@@ -0,0 +1,14 @@
+package com.example.diamondcastapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CreateAccount2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_account2);
+    }
+}
