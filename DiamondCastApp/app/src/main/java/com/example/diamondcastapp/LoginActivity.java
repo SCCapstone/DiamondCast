1729d9987dd19e00083b0c3f5c5b7e370436211d
Frank Habersham <frankh@email.sc.edu>
2021-11-07 15:35:24 -0500

    changed MainActivity to LoginActivity

--- DiamondCastApp/app/src/main/java/com/example/diamondcastapp/LoginActivity.java
+++ DiamondCastApp/app/src/main/java/com/example/diamondcastapp/LoginActivity.java
@@ -4,14 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class LoginActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        //Branch merge for Justin
-        //test push
-        //Daniel Testing commit
+        setContentView(R.layout.activity_login);
     }
 }
