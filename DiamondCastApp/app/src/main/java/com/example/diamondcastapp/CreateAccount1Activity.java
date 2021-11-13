5e1c97d42847c5a0e30db00eb35494bf5fa3037d
Frank Habersham <frankh@email.sc.edu>
2021-11-13 15:40:54 -0500

    edited login and create account activity

--- DiamondCastApp/app/src/main/java/com/example/diamondcastapp/CreateAccount1Activity.java
+++ DiamondCastApp/app/src/main/java/com/example/diamondcastapp/CreateAccount1Activity.java
@@ -2,13 +2,83 @@ package com.example.diamondcastapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+import com.google.android.material.snackbar.Snackbar;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class CreateAccount1Activity extends AppCompatActivity {
+    private static final String regex = "^[a-zA-Z0-9_+&*-]+(?:\\.[a-zA-Z0-9_+&*-]+)*@(?:[a-zA-Z0-9-]+\\.)+[a-zA-Z]{2,7}$";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_account1);
     }
+
+    public void goToCreateAccount2Activity (View view) {
+        //if (creatingAccount()) {
+            Intent intent = new Intent(this, CreateAccount2Activity.class);
+            startActivity(intent);
+        //}
+    }
+
+    public void goToLoginActivity (View view) {
+        Intent intent = new Intent(this, LoginActivity.class);
+        startActivity(intent);
+    }
+
+    public boolean creatingAccount() {
+        boolean createdAccount = false;
+
+        EditText firstNameInput = findViewById(R.id.createAccount1FirstNameInput);
+        EditText lastNameInput = findViewById(R.id.createAccount1LastNameInput);
+        EditText emailInput = findViewById(R.id.createAccount1EmailInput);
+        EditText usernameInput = findViewById(R.id.createAccount1UsernameInput);
+        EditText passwordInput = findViewById(R.id.createAccount1PasswordInput);
+        EditText comfirmPasswordInput = findViewById(R.id.createAccount1ComfirmPasswordInput);
+
+        String firstName = firstNameInput.getText().toString();
+        String lastName = lastNameInput.getText().toString();
+        String email = emailInput.getText().toString();
+        String username = usernameInput.getText().toString();
+        String password = passwordInput.getText().toString();
+        String comfirmPassword = comfirmPasswordInput.getText().toString();
+
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(email);
+
+        //Try to find email and username from server here
+
+        if(firstName == "") {
+            Snackbar.make(findViewById(R.id.createAccount1FirstNameInput), "Enter your first name", Snackbar.LENGTH_SHORT).show();
+        } else if(lastName == "") {
+            Snackbar.make(findViewById(R.id.createAccount1LastNameInput), "Enter your last name", Snackbar.LENGTH_SHORT).show();
+        } else if(email == "") {
+            Snackbar.make(findViewById(R.id.createAccount1EmailInput), "Enter an email", Snackbar.LENGTH_SHORT).show();
+        } else if(!matcher.matches()){
+            Snackbar.make(findViewById(R.id.createAccount1EmailInput), "Email is invalid", Snackbar.LENGTH_SHORT).show();
+        } else if(false) {
+            Snackbar.make(findViewById(R.id.createAccount1EmailInput), "Email already in use", Snackbar.LENGTH_SHORT).show();
+        } else if(username == "") {
+            Snackbar.make(findViewById(R.id.createAccount1UsernameInput), "Enter a username", Snackbar.LENGTH_SHORT).show();
+        } else if(false) {
+            Snackbar.make(findViewById(R.id.createAccount1UsernameInput), "Username already in use", Snackbar.LENGTH_SHORT).show();
+        } else if(password == "") {
+            Snackbar.make(findViewById(R.id.createAccount1PasswordInput), "Enter a password", Snackbar.LENGTH_SHORT).show();
+        } else if(comfirmPassword == "") {
+            Snackbar.make(findViewById(R.id.createAccount1ComfirmPasswordInput), "Confirm your password", Snackbar.LENGTH_SHORT).show();
+        } else if(comfirmPassword != password) {
+            Snackbar.make(findViewById(R.id.createAccount1ComfirmPasswordInput), "Passwords do not match", Snackbar.LENGTH_SHORT).show();
+        } else {
+            createdAccount = true;
+        }
+        return createdAccount;
+    }
+
 }
--- DiamondCastApp/app/src/main/java/com/example/diamondcastapp/LoginActivity.java
+++ DiamondCastApp/app/src/main/java/com/example/diamondcastapp/LoginActivity.java
@@ -5,6 +5,9 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
+
+import com.google.android.material.snackbar.Snackbar;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -15,8 +18,10 @@ public class LoginActivity extends AppCompatActivity {
     }
 
     public void goToHomeScreenActivity (View view) {
-        Intent intent = new Intent(this, HomeScreenActivity.class);
-        startActivity(intent);
+        //if(loggingIn()) {
+            Intent intent = new Intent(this, HomeScreenActivity.class);
+            startActivity(intent);
+        //}
     }
 
     public void goToForgotPasswordActivity (View view) {
@@ -27,5 +32,26 @@ public class LoginActivity extends AppCompatActivity {
     public void goToCreateAccount1Activity (View view) {
         Intent intent = new Intent(this, CreateAccount1Activity.class);
         startActivity(intent);
+    }
+
+    private boolean loggingIn() {
+        boolean loggedIn = false;
+        EditText usernameInput = findViewById(R.id.loginUsernameInput);
+        EditText passwordInput = findViewById(R.id.loginPasswordInput);
+        String username = usernameInput.getText().toString();
+        String password = passwordInput.getText().toString();
+
+        //Try to find username and password from server here
+
+        if (false) {
+            Snackbar.make(findViewById(R.id.loginUsernameInput), "Username not found", Snackbar.LENGTH_SHORT).show();
+        } else {
+            if (false) {
+                Snackbar.make(findViewById(R.id.loginPasswordInput), "Incorrect password", Snackbar.LENGTH_SHORT).show();
+            } else {
+                loggedIn = true;
+            }
+        }
+        return loggedIn;
     }
 }
