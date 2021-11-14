f2a30724bbba720cf3eb60ee2fbe5a77ac21e418
Justin Cordell <cordellj@email.sc.edu>
2021-11-13 20:23:37 -0500

    searching activity

---
+++ DiamondCastApp/app/src/main/java/com/example/diamondcastapp/MainActivity.java
@@ -0,0 +1,10 @@
+package com.example.diamondcastapp;
+
+import android.widget.ArrayAdapter;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity{
+
+
+}
