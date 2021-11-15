08cf1c3630a79c14ed96696732f64ee5d1653852
Jackson Trigiani <jacktrig@email.sc.edu>
2021-11-14 19:53:11 -0500

    added calendar class and stubs

---
+++ DiamondCastApp/app/src/main/java/Calendar.java
@@ -0,0 +1,10 @@
+public class Calendar {
+    protected User user;
+    //need to think about use of this class; should this be activity?
+    public void displayAppointments() {
+        //TODO
+    }
+    public void updateAppointments() {
+        //TODO
+    }
+}
