ebc51add72686e5c1ba9c234532d65b96b282de0
Jackson Trigiani <jacktrig@email.sc.edu>
2021-11-21 18:31:41 -0500

    connected app to firebase and realtime database

--- DiamondCastApp/app/src/main/java/Appointment.java
+++ DiamondCastApp/app/src/main/java/Appointment.java
@@ -1,15 +1,20 @@
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
 import java.util.Date;
 
 public class Appointment {
     public String id;
     public String title;
     public Date date;
-    public String time;
     public String service;
     public boolean active;
 
     public Appointment() {
-        //TODO
+        this.id = "0";
+        this.title = title;
+        this.date = new Date("yyyy-MM-dd HH:mm:ss");
+        this.service = "service";
+        this.active = true;
     }
     public String getAppointmentID() {
         return this.id;
@@ -19,9 +24,6 @@ public class Appointment {
     }
     public Date getAppointmentDate() {
         return this.date;
-    }
-    public String getAppointmentTime() {
-        return this.time;
     }
     public String getAppointmentService() {
         return this.service;
@@ -37,9 +39,6 @@ public class Appointment {
     }
     public void setAppointmentDate(Date date) {
         this.date = date;
-    }
-    public void setAppointmentTime(String time) {
-        this.time = time;
     }
     public void setAppointmentService(String service) {
         this.service = service;
