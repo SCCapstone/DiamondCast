72baccb9483a34bf97103384a41d5f8ddfbb214d
dm721 <dmckenna721@gmail.com>
2021-11-28 21:56:46 -0500

    changed user.java class and added register userclass

---
+++ DiamondCastApp/app/src/main/java/com/example/diamondcastapp/registerUser.java
@@ -0,0 +1,12 @@
+package main.java.com.example.diamondcastapp;
+
+/**************************************************************
+    *Class for registering a user with firebase database 
+    *@author Daniel McKenna 
+    *@version 1.0.0 11-28-21
+    *Copyright tbd
+***************************************************************/
+
+public class RegisterUser {
+
+}
