aee94c4221e07febf310dc5cdda7385db7ea7ff6
Jackson Trigiani <jacktrig@email.sc.edu>
2022-01-15 16:03:46 -0500

    added text to appointment activity to start

--- DiamondCastApp/app/src/main/java/com/example/diamondcastapp/ClientHomeScreenActivity.java
+++ DiamondCastApp/app/src/main/java/com/example/diamondcastapp/ClientHomeScreenActivity.java
@@ -2,7 +2,9 @@ package com.example.diamondcastapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class ClientHomeScreenActivity extends AppCompatActivity {
 
@@ -10,5 +12,9 @@ public class ClientHomeScreenActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_client_home_screen);
+    }
+    public void goToAppointmentSchedulerActivity (View view) {
+        Intent intent = new Intent(this, AppointmentScheduler.class);
+        startActivity(intent);
     }
 }
