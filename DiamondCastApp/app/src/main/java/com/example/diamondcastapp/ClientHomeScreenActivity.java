ad264c6cefc46575850d12868d20dd58972a5e33
Jackson Trigiani <jacktrig@email.sc.edu>
2022-01-15 16:39:33 -0500

    starting appointment scheduling activity

--- DiamondCastApp/app/src/main/java/com/example/diamondcastapp/ClientHomeScreenActivity.java
+++ DiamondCastApp/app/src/main/java/com/example/diamondcastapp/ClientHomeScreenActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 
 public class ClientHomeScreenActivity extends AppCompatActivity {
 
@@ -12,9 +13,21 @@ public class ClientHomeScreenActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_client_home_screen);
+
+        Button appointmentScheduler = (Button) findViewById(R.id.goToAppointmentBtn);
+
+        appointmentScheduler.setOnClickListener( new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+               goToAppointmentSchedulerActivity();
+            }
+        });
     }
-    public void goToAppointmentSchedulerActivity (View view) {
-        Intent intent = new Intent(this, AppointmentScheduler.class);
+    public void goToAppointmentSchedulerActivity() {
+        Intent intent = new Intent(this, AppointmentSchedulerActivity.class);
         startActivity(intent);
     }
+
+
 }
