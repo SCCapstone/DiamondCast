4b2ba2cd4baf4834e48f4cbaf479c72368376c47
Justin Cordell <cordellj@email.sc.edu>
2022-01-27 17:47:17 -0500

    implemented email verification for users #41

--- DiamondCastApp/app/src/main/java/com/example/diamondcastapp/CreateAccount2Activity.java
+++ DiamondCastApp/app/src/main/java/com/example/diamondcastapp/CreateAccount2Activity.java
@@ -1,15 +1,21 @@
 package com.example.diamondcastapp;
 
+import static android.content.ContentValues.TAG;
+
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Spinner;
+import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.android.material.snackbar.Snackbar;
 import com.google.firebase.auth.AuthResult;
@@ -17,6 +23,7 @@ import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.core.Tag;
 
 public class CreateAccount2Activity extends AppCompatActivity {
 
@@ -69,6 +76,19 @@ public class CreateAccount2Activity extends AppCompatActivity {
                     @Override
                     public void onComplete(@NonNull Task<AuthResult> task) {
                         if(task.isSuccessful()){
+                            FirebaseUser fUser = fAuth.getCurrentUser();
+                            fUser.sendEmailVerification().addOnSuccessListener(new OnSuccessListener<Void>(){
+                              @Override
+                              public void onSuccess(Void aVoid) {
+                                  Toast.makeText(CreateAccount2Activity.this, "Verification Email has been Sent.", Toast.LENGTH_SHORT).show();
+                              }
+                            }).addOnFailureListener(new OnFailureListener() {
+                                @Override
+                                public void onFailure(@NonNull Exception e) {
+                                    Log.d(TAG, "onFailure: Email not sent" +e.getMessage());
+                                }
+                            });
+
                             String Uid = FirebaseAuth.getInstance().getCurrentUser().getUid();
                             newUser.setId(Uid);
                             FirebaseDatabase.getInstance().getReference("Users")
