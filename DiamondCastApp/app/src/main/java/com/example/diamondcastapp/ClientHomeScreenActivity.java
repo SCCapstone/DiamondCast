b85027c987f83071f547edb2383f835e85e0e8a8
= <=>
2022-02-04 22:52:46 -0500

    not working

--- DiamondCastApp/app/src/main/java/com/example/diamondcastapp/ClientHomeScreenActivity.java
+++ DiamondCastApp/app/src/main/java/com/example/diamondcastapp/ClientHomeScreenActivity.java
@@ -1,6 +1,12 @@
 package com.example.diamondcastapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.fragment.NavHostFragment;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
 
 import android.content.Intent;
 import android.os.Bundle;
@@ -8,14 +14,21 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
 
+import com.google.android.material.navigation.NavigationView;
+
 public class ClientHomeScreenActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_client_home_screen);
-
         Button appointmentScheduler = (Button) findViewById(R.id.goToAppointmentBtn);
+
+        //Navigation Menu
+        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_navigation_menu);
+        NavigationView navView = findViewById(R.id.nav_view);
+        NavigationUI.setupWithNavController(navView, navController);
+
         appointmentScheduler.setOnClickListener( new View.OnClickListener() {
             @Override
             public void onClick(View v) { goToAppointmentSchedulerActivity(); }
