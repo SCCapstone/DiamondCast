25bc027bf1e71e8a4cdd63f8eb95dbc52d4a9bf7
Justin Cordell <cordellj@email.sc.edu>
2022-02-27 13:51:44 -0500

    Client home screen shows upcoming appointments #8

--- DiamondCastApp/app/src/main/java/com/example/diamondcastapp/ClientHomeScreenActivity.java
+++ DiamondCastApp/app/src/main/java/com/example/diamondcastapp/ClientHomeScreenActivity.java
@@ -1,6 +1,9 @@
 package com.example.diamondcastapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
 import android.os.Bundle;
@@ -8,13 +11,48 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+
 public class ClientHomeScreenActivity extends AppCompatActivity {
+
+    private RecyclerView resultList;
+    private DatabaseReference databaseReference;
+    private HomeScreenAdapter homeScreenAdapter;
+    private ArrayList<Appointment> userAppt;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_client_home_screen);
 
+        //Appointment RecyclerView
+        resultList = findViewById(R.id.appointmentResult);
+        resultList.setHasFixedSize(true);
+        resultList.setLayoutManager(new LinearLayoutManager(this));
+        userAppt = new ArrayList<>();
+        homeScreenAdapter = new HomeScreenAdapter(userAppt, this);
+        resultList.setAdapter(homeScreenAdapter);
+
+        databaseReference = FirebaseDatabase.getInstance().getReference().child("Appointments");
+        databaseReference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                for(DataSnapshot dataSnapshot : snapshot.getChildren()) {
+                    Appointment appointment= dataSnapshot.getValue(Appointment.class);
+                    userAppt.add(appointment);
+                }
+            }
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+            }
+        });
         Button appointmentScheduler = (Button) findViewById(R.id.goToAppointmentBtn);
         appointmentScheduler.setOnClickListener( new View.OnClickListener() {
             @Override
