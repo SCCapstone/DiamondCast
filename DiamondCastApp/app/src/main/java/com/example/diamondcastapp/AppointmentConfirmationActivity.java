7a9aabd7d82e24d9871105bd1883ef8ea4d18ceb
unknown <jacktrig@email.sc.edu>
2022-03-22 18:18:09 -0400

    moved date and time picker to the appointment confirmation page #64. set up different date picker.

--- DiamondCastApp/app/src/main/java/com/example/diamondcastapp/AppointmentConfirmationActivity.java
+++ DiamondCastApp/app/src/main/java/com/example/diamondcastapp/AppointmentConfirmationActivity.java
@@ -3,8 +3,10 @@ package com.example.diamondcastapp;
 import androidx.annotation.NonNull;
 import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.DialogFragment;
 
 import android.app.AlertDialog;
+import android.app.DatePickerDialog;
 import android.app.TimePickerDialog;
 import android.content.Intent;
 import android.os.Build;
@@ -12,6 +14,7 @@ import android.os.Bundle;
 import android.os.Parcelable;
 import android.view.View;
 import android.widget.Button;
+import android.widget.DatePicker;
 import android.widget.TextView;
 import android.widget.TimePicker;
 
@@ -25,7 +28,13 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
+import java.text.SimpleDateFormat;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+import java.time.format.FormatStyle;
 import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
 import java.util.Locale;
 
 public class AppointmentConfirmationActivity extends AppCompatActivity {
@@ -46,6 +55,11 @@ public class AppointmentConfirmationActivity extends AppCompatActivity {
     Appointment appointment;
     DatabaseReference databaseReference;
 
+    private DatePickerDialog datePickerDialog;
+
+    private Button selectDateButton;
+
+
     private AppointmentList appointmentList;
 
     @RequiresApi(api = Build.VERSION_CODES.O)
@@ -53,6 +67,7 @@ public class AppointmentConfirmationActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_appointment_confirmation);
+        initDatePicker();
         //get data passed from previous activity
         Intent intent = getIntent();
         selectedDate = intent.getStringExtra("selectedDate");
@@ -68,6 +83,9 @@ public class AppointmentConfirmationActivity extends AppCompatActivity {
         displaySelectedContractor = findViewById(R.id.displaySelectedContractorConfirm);
         displaySelectedServices = findViewById(R.id.displaySelectedServicesConfirm);
         selectTimeButton = findViewById(R.id.chooseAppointmentTimeButton);
+        selectTimeButton.setText(getCurrentTime());
+        selectDateButton = findViewById(R.id.chooseAppointmentDateButton);
+        selectDateButton.setText(getTodaysDate());
         String selectedServicesDisplayString = String.join(", ", selectedServicesList);
 
         displaySelectedServices.setText(selectedServicesDisplayString);
@@ -125,11 +143,98 @@ public class AppointmentConfirmationActivity extends AppCompatActivity {
             }
         });
 
+    }
+    public void openTimePicker(View view) {
+        TimePickerDialog.OnTimeSetListener onTimeSetListener = new TimePickerDialog.OnTimeSetListener() {
+            @RequiresApi(api = Build.VERSION_CODES.O)
+            @Override
+            public void onTimeSet(TimePicker timePicker, int selectedHour, int selectedMinute){
+                hour = selectedHour;
+                minute = selectedMinute;
+                LocalTime time = LocalTime.of(hour,minute);
+                String timeFormat = DateTimeFormatter.ofLocalizedTime(FormatStyle.SHORT).format(time);
+                selectTimeButton.setText(timeFormat);
+            }
+        };
 
+        int style = AlertDialog.THEME_HOLO_DARK;
 
+        TimePickerDialog timePickerDialog = new TimePickerDialog(this, style, onTimeSetListener, hour, minute,false);
 
+        timePickerDialog.setTitle("SelectTime");
+        timePickerDialog.show();
 
     }
+
+    private void initDatePicker() {
+        DatePickerDialog.OnDateSetListener dateSetListener = new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker datePicker, int year, int month, int day) {
+                month = month+1;
+                String date = makeDateString(day, month, year);
+                selectDateButton.setText(date);
+            }
+        };
+
+        Calendar cal = Calendar.getInstance();
+        int year = cal.get(Calendar.YEAR);
+        int month = cal.get(Calendar.MONTH);
+        int day = cal.get(Calendar.DAY_OF_MONTH);
+
+        int style = AlertDialog.THEME_HOLO_LIGHT;
+
+        datePickerDialog = new DatePickerDialog(this, style, dateSetListener, year, month, day);
+    }
+
+    private String makeDateString(int day, int month, int year) {
+        return getMonthFormat(month) + "/" + day + "/" + year;
+    }
+    @RequiresApi(api = Build.VERSION_CODES.O)
+    private String getCurrentTime() {
+        LocalTime time = LocalTime.now();
+        String timeFormat = DateTimeFormatter.ofLocalizedTime(FormatStyle.SHORT).format(time);
+        return timeFormat;
+    }
+    private String getTodaysDate() {
+        Calendar cal = Calendar.getInstance();
+        int year = cal.get(Calendar.YEAR);
+        int month = cal.get(Calendar.MONTH);
+        month++;
+        int day = cal.get(Calendar.DAY_OF_MONTH);
+        return makeDateString(day,month,year);
+    }
+    private String getMonthFormat(int month) {
+        if(month == 1)
+            return "JAN";
+        if(month == 2)
+            return "FEB";
+        if(month == 3)
+            return "MAR";
+        if(month == 4)
+            return "APR";
+        if(month == 5)
+            return "MAY";
+        if(month == 6)
+            return "JUN";
+        if(month == 7)
+            return "JUL";
+        if(month == 8)
+            return "AUG";
+        if(month == 9)
+            return "SEP";
+        if(month == 10)
+            return "OCT";
+        if(month == 11)
+            return "NOV";
+        if(month == 12)
+            return "DEC";
+        return "INV";
+    }
+    public void openDatePicker(View view) {
+        datePickerDialog.show();
+    }
+
+
     public void goToHomeScreenActivity() {
         String currentUserId = FirebaseAuth.getInstance().getCurrentUser().getUid();
         FirebaseDatabase.getInstance().getReference("Clients")
@@ -154,24 +259,7 @@ public class AppointmentConfirmationActivity extends AppCompatActivity {
             }
         });
     }
-    public void openTimePicker(View view) {
-        TimePickerDialog.OnTimeSetListener onTimeSetListener = new TimePickerDialog.OnTimeSetListener() {
-            @Override
-            public void onTimeSet(TimePicker timePicker, int selectedHour, int selectedMinute){
-                hour = selectedHour;
-                minute = selectedMinute;
-                selectTimeButton.setText(String.format(Locale.getDefault(), "%02d:%02d", hour, minute));
-            }
-        };
 
-        int style = AlertDialog.THEME_HOLO_DARK;
-
-        TimePickerDialog timePickerDialog = new TimePickerDialog(this, style, onTimeSetListener, hour, minute,true);
-
-        timePickerDialog.setTitle("SelectTime");
-        timePickerDialog.show();
-
-    }
     private void goToClientHomeScreenActivity() {
         Intent intent = new Intent(this, ClientHomeScreenActivity.class);
         intent.putExtra("appointment", appointment);
