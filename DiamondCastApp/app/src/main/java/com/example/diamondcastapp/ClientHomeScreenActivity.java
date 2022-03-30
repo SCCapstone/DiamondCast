2008b163e0d75920e111ba86e74fc52ac7d853ee
unknown <jacktrig@email.sc.edu>
2022-03-30 16:47:55 -0400

    testing if setup works on home screen #62

--- DiamondCastApp/app/src/main/java/com/example/diamondcastapp/ClientHomeScreenActivity.java
+++ DiamondCastApp/app/src/main/java/com/example/diamondcastapp/ClientHomeScreenActivity.java
@@ -11,6 +11,7 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
 
+import com.example.diamondcastapp.databinding.ActivityClientHomeScreenBinding;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
@@ -20,19 +21,21 @@ import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
 
-public class ClientHomeScreenActivity extends AppCompatActivity {
+public class ClientHomeScreenActivity extends NavigationDrawerActivity {
     private HomeScreenAppointmentAdapter adapter;
     private RecyclerView homeScreenApptList;
     public Appointment createdAppointment;
     private ArrayList<Appointment> list;
     private DatabaseReference databaseReference;
     private AppointmentList appointmentList;
+    ActivityClientHomeScreenBinding activityClientHomeScreenBinding;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_client_home_screen);
+        activityClientHomeScreenBinding = ActivityClientHomeScreenBinding.inflate(getLayoutInflater());
+        setContentView(activityClientHomeScreenBinding.getRoot());
 
 
 
--- DiamondCastApp/app/src/main/java/com/example/diamondcastapp/NavigationDrawerActivity.java
+++ DiamondCastApp/app/src/main/java/com/example/diamondcastapp/NavigationDrawerActivity.java
@@ -1,14 +1,50 @@
 package com.example.diamondcastapp;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.FrameLayout;
 
-public class NavigationDrawerActivity extends AppCompatActivity {
+import com.google.android.material.navigation.NavigationView;
+
+
+public class NavigationDrawerActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+
+    DrawerLayout drawerLayout;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_navigation_drawer);
+    public void setContentView(View view) {
+        drawerLayout = (DrawerLayout) getLayoutInflater().inflate(R.layout.activity_navigation_drawer,null);
+        FrameLayout container = drawerLayout.findViewById(R.id.activityContainer);
+        container.addView(view);
+        super.setContentView(drawerLayout);
+
+        Toolbar toolbar = drawerLayout.findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        NavigationView navigationView = drawerLayout.findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawerLayout, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+
+        drawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
     }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        return false;
+    }
+
+    protected void allocateActivityTitle(String title) {
+        if(getSupportActionBar() != null)
+            getSupportActionBar().setTitle(title);
+    }
+
 }
