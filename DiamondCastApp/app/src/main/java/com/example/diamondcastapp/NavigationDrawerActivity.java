b785c8aff100a8ee909a60d20cd85dc569b9dcb7
Justin Cordell <cordellj@email.sc.edu>
2022-04-12 15:27:10 -0400

    Removed ability for contractor/agent to schedule appointments, closes #100

--- DiamondCastApp/app/src/main/java/com/example/diamondcastapp/NavigationDrawerActivity.java
+++ DiamondCastApp/app/src/main/java/com/example/diamondcastapp/NavigationDrawerActivity.java
@@ -10,6 +10,7 @@ import androidx.appcompat.widget.Toolbar;
 import android.content.Intent;
 import android.os.Bundle;
 import android.provider.Settings;
+import android.util.Log;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.FrameLayout;
@@ -29,7 +30,7 @@ public class NavigationDrawerActivity extends AppCompatActivity implements Navig
 
     FirebaseUser fUser;
     DatabaseReference dReference;
-    String userID;
+    String userID, userType;
 
     DrawerLayout drawerLayout;
 
@@ -52,44 +53,69 @@ public class NavigationDrawerActivity extends AppCompatActivity implements Navig
         NavigationView navigationView = drawerLayout.findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
 
+        //Remove appointment creation for Contractors / Agents
+        String uid = FirebaseAuth.getInstance().getCurrentUser().getUid();
+        DatabaseReference rootRef = FirebaseDatabase.getInstance().getReference();
+        DatabaseReference usersRef = rootRef.child("Users");
+        DatabaseReference current_userRef = usersRef.child(uid);
+        ValueEventListener valueEventListener = new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                userType = dataSnapshot.child("userType").getValue(String.class);
+                Log.d("TAG", userType);
+
+                if (userType.equals("Contractor") || (userType.equals("Agent"))) {
+                    navigationView.getMenu().findItem(R.id.nav_appointment).setVisible(false);
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+                Log.d("TAG", databaseError.getMessage());
+            }
+        };
+        current_userRef.addListenerForSingleValueEvent(valueEventListener);
+
+
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawerLayout, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
 
         drawerLayout.addDrawerListener(toggle);
         toggle.syncState();
+
     }
 
     @Override
     public boolean onNavigationItemSelected(@NonNull MenuItem item) {
         drawerLayout.closeDrawer(GravityCompat.START);
-
-        switch (item.getItemId()) {
-            case R.id.nav_home:
-                startActivity(new Intent(this, ClientHomeScreenActivity.class));
-                overridePendingTransition(0, 0);
-                break;
-            case R.id.nav_search:
-                startActivity(new Intent(this, SearchingActivity.class));
-                overridePendingTransition(0, 0);
-                break;
-            case R.id.nav_appointment:
-                startActivity(new Intent(this, AppointmentConfirmationActivity.class));
-                overridePendingTransition(0, 0);
-                break;
-            case R.id.nav_messaging:
-                startActivity(new Intent(this, MessagingActivity.class));
-                overridePendingTransition(0, 0);
-                break;
-            case R.id.nav_profile:
-                startActivity(new Intent(this, ProfileActivity.class));
-                overridePendingTransition(0, 0);
-                break;
-            case R.id.nav_settings:
-                startActivity(new Intent(this, SettingsActivity.class));
-                overridePendingTransition(0, 0);
-                break;
+            switch (item.getItemId()) {
+                case R.id.nav_home:
+                    startActivity(new Intent(this, ClientHomeScreenActivity.class));
+                    overridePendingTransition(0, 0);
+                    break;
+                case R.id.nav_search:
+                    startActivity(new Intent(this, SearchingActivity.class));
+                    overridePendingTransition(0, 0);
+                    break;
+                case R.id.nav_appointment:
+                    startActivity(new Intent(this, AppointmentConfirmationActivity.class));
+                    overridePendingTransition(0, 0);
+                    break;
+                case R.id.nav_messaging:
+                    startActivity(new Intent(this, MessagingActivity.class));
+                    overridePendingTransition(0, 0);
+                    break;
+                case R.id.nav_profile:
+                    startActivity(new Intent(this, ProfileActivity.class));
+                    overridePendingTransition(0, 0);
+                    break;
+                case R.id.nav_settings:
+                    startActivity(new Intent(this, SettingsActivity.class));
+                    overridePendingTransition(0, 0);
+                    break;
+            }
+            return false;
         }
-        return false;
-    }
+
 
     protected void allocateActivityTitle(String title) {
         if (getSupportActionBar() != null)
@@ -132,5 +158,8 @@ public class NavigationDrawerActivity extends AppCompatActivity implements Navig
         startActivity(intent);
     }
 
+/*
+
+*/
 
 }
